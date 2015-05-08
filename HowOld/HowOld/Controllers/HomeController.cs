b3f7174c48953fcdb442786b5439ec1e2db677a8
Bean <Bean.B.Deng@newegg.com>
2015-05-08 17:21:48 +0800

    Add HowOld Demo and site(undone)

---
+++ HowOld/HowOld/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace HowOld.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ HowOld/HowOld/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NiDuoDa.net.MvcApplication" Language="C#" %>
